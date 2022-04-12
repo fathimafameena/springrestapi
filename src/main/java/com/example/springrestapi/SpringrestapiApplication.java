2855bfaae8de7ec84cf347d81c06e4518854b359
Fameena <fameenasharfudeen@gmail.com>
2022-04-12 16:55:54 -0500

    this is my first commit

---
+++ src/main/java/com/example/springrestapi/SpringrestapiApplication.java
@@ -0,0 +1,16 @@
+package com.example.springrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrestapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringrestapiApplication.class, args);
+
+
+        System.out.println("i am a spring boot application:");
+    }
+
+}
---
+++ src/main/java/com/example/springrestapi/controller/startController.java
@@ -0,0 +1,22 @@
+package com.example.springrestapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/fameena")
+public class startController {
+
+    @GetMapping(value = "/{name}")
+    public String fetchapplication(@PathVariable String name) {
+
+        System.out.println("the main application is " + name);
+        return name;
+
+
+    }
+
+
+}
